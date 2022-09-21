e8bcdab3b59ae36817fe6f53f99792551bf2b191
Adrià <87275440+KrxnoS@users.noreply.github.com>
2022-09-21 12:22:19 +0200

    Add files via upload

---
+++ DAW_ DWES _Molina_Palomino_Adrià.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <?php for($i=1;$i<=10;$i++): ?>
+        <tr>
+            <td><?php echo "Si estamos en 2º de DAW";?></td>
+        </tr>
+      <?php endfor; ?>
+    </table>
+</body>
+</html>
